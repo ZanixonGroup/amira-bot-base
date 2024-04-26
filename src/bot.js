1d2bd293e46d71e2bb0fecceec5b40e5548b3738
zevanoo <flashingyudha@gmail.com>
2024-04-26 19:10:37 +0700

    Patch v0.0.10

--- src/bot.js
+++ src/bot.js
@@ -34,10 +34,7 @@ async function start() {
       console.log(">", "Pairing Code:".info, pairingCode.warn);
     }, 30000);
   }
-<<<<<<< HEAD
   global.client = client;
-=======
->>>>>>> refs/remotes/origin/master
   await loadEvents(client, "Events");
   
   // session manager
