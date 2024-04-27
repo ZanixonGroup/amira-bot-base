30667f19fc89c460c109b3daa7a50fee3caa7447
zevanoo <flashingyudha@gmail.com>
2024-04-27 21:06:15 +0700

    Patch v0.0.16

--- src/handler/Commands.js
+++ src/handler/Commands.js
@@ -14,8 +14,22 @@ async function loadCommands(commandsDirectory, logs) {
       const options = {
         name: command?.name,
         command: command?.command,
+        options: {
+        	isAdmin: command?.isAdmin ? command?.isAdmin : false,
+            isBotAdmin: command?.isBotAdmin ? command?.isBotAdmin : false,
+            isPremium: command?.isPremium ? command?.isPremium : false,
+            isOwner: command?.isOwner ? command?.isOwner : false,
+            isGroup: command?.isGroup ? command?.isGroup : false,
+            isPrivate: command?.isPrivate ? command?.isPrivate : false,
+            nonPrefix: command?.nonPrefix ? command?.nonPrefix : false,
+        },
+        disable: {
+        	status: ? command?.disable?.status ? command?.disable?.status : false,
+            message: command?.disable?.message ? command?.disable?.message : false,
+        },
         code: command?.code ? command?.code : () => {}
       };
+      console.log(command)
       Commands.set(command?.name, options);
     })
     return Commands;
