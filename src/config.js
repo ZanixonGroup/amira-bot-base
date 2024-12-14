7144d70a7ed2795c69cbe31bc5053a4437ee115d
irvankur <irfancandra232@gmail.com>
2024-12-14 23:05:12 +0700

    config.js

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ global.botNumber = "62856971039021";
 global.bot = {
   public: true,
   antiCall: true,
-  owner: ["6285697103902", "6289520306297", "6283897390164"],
+  owner: ["6285236216063"],
   sessionName: "session",
   prefix: /^[.]/i
 }
